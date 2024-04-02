deda6c426729dbb3649c6af1d731465ca20abfad
Prakhar <89300995+bas-bhai-account-khol-de@users.noreply.github.com>
2024-04-02 17:45:04 +0530

    added sift

--- bestkeyPointSelector.py
+++ bestkeyPointSelector.py
@@ -7,15 +7,20 @@ import numpy as np
 
 
 
+matcher = cv2.DescriptorMatcher_create(cv2.DescriptorMatcher_BRUTEFORCE)
+def getBestpoints(img_path,randomise = False):
+    
+        
+    image= cv2.imread(img_path)
+    if(randomise):
+        image = randomiseImage(image)
 
-def getBestpoints(img_path):
-    img= cv2.imread(img_path)
-    print("loaded Image " +str( img.shape) )
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    print("loaded Image " +str( image.shape) )
+    img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     kp,desc =findkeypoints(img,False)
     randomiseImage(img,False)
 
-    matcher = cv2.DescriptorMatcher_create(cv2.DescriptorMatcher_BRUTEFORCE)
+    
     for i in range(10):
         randomImage  =  randomiseImage(img,False)
         kpt,desct = findkeypoints(randomImage,False)
@@ -23,25 +28,40 @@ def getBestpoints(img_path):
         nkp = []
         ndesc =[]
         good_matches = []
-        ratio_thresh = 0.75  # Lowe's ratio test
+        ratio_thresh = 0.9  # Lowe's ratio test
         for m, n in matches:
             if m.distance < ratio_thresh * n.distance:
                 good_matches.append(m)
         for m in good_matches:
             nkp.append(kp[m.queryIdx])
             ndesc.append(desc[m.queryIdx])
-            
         kp=tuple(nkp)
         desc = np.array(ndesc)
         print(len(kp))
         pass
+    drawKp(image,img_path,kp,True)
 
 
 
+
+
+def drawKp(img,img_path,kp ,show =False):
+    kps,dsc = findkeypoints(img)
+    keypoint_image = cv2.drawKeypoints(img, kp, None, color=(0,255,0), flags=0)
+    # Display the image with keypoint
+    if show:
+        cv2.imshow('Warped', keypoint_image)
+        k = cv2.waitKey(0) & 0xFF
+        cv2.destroyAllWindows()
+        if k != 27:
+            print("path is "+img_path)
+            getBestpoints(img_path,True)
+            
 
 
 def findkeypoints(gray_image , show =False):
-    orb = cv2.ORB_create(1000)
+    # orb = cv2.ORB_create(1000)
+    orb = cv2.SIFT_create(500)
     # Detect keypoints
     keypoints ,  discriptor = orb.detectAndCompute (gray_image, None)
     # Draw keypoints
@@ -55,10 +75,10 @@ def findkeypoints(gray_image , show =False):
 
 
 # create random homography of image
-def randomiseImage(image ,show =False):
+def randomiseImage(image ,show =False,shift =200):
     h, w = image.shape[:2]
     pts_src = np.array([[0, 0], [w-1, 0], [w-1, h-1], [0, h-1]], dtype=float)
-    pts_dst = pts_src + np.random.rand(4, 2) * 400 - 200  # Adjust the range according to your needs
+    pts_dst = pts_src + np.random.rand(4, 2) * shift - (shift/2)  # Adjust the range according to your needs
     H, _ = cv2.findHomography(pts_src, pts_dst)
     warped_image = cv2.warpPerspective(image, H, (w, h))
     if show:
